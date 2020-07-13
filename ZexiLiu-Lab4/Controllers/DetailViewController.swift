8d69d7f5fd94e8857c52543bfe5a55e809a2521e
mikeliu <liuzexi@wustl.edu>
2020-07-12 19:09:10 -0500

    connect controller with the storyboard

---
+++ ZexiLiu-Lab4/Controllers/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  ZexiLiu-Lab4
+//
+//  Created by Mike Liu on 7/11/20.
+//  Copyright © 2020 Mike Liu. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var voteAvgLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var overviewText: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
